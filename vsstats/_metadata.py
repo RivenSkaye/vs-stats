4329e0c1886506f72e02152dc913f41ef419ea49
Riven Skaye <pretender0906@gmail.com>
2022-07-22 20:45:43 +0200

    Package metadata

---
+++ vsstats/_metadata.py
@@ -0,0 +1,12 @@
+"""Functions for collecting statistics on (variable) video"""
+
+__version__ = "0.0.1"
+
+__author__ = "RivenSkaye <riven@tae.moe>"
+__maintainer__ = __author__
+
+__author_name__, __author_email__ = [x[:-1] for x in __author__.split("<")]
+__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split("<")]
+
+if __name__ == "__github__":
+    print(__version__)
