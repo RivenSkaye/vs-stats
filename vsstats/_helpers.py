eacb1cc4ff11abb4dccf879e3bab0c1866b3681c
Riven Skaye <pretender0906@gmail.com>
2022-08-10 14:05:25 +0200

    Add a get_res

--- vsstats/_helpers.py
+++ vsstats/_helpers.py
@@ -4,14 +4,16 @@ from .types import Resolution
 
 
 def _get_res(
-    res: Optional[Union[Resolution, Sequence]], width: Optional[int], height: Optional[int],
-    func: str
+    res: Union[Resolution, Sequence, None] = None,
+    width: Optional[int] = None,
+    height: Optional[int] = None,
+    func: str = "User-defined function"
 ) -> Optional[Resolution]:
     if res is None and width is None and height is None:
         return None
     if res is not None:
         return res if isinstance(res, Resolution) else Resolution(res[0], res[1])
     if width is not None and height is not None:
-        return Resolution(width, height)
+        return Resolution(width, height) if width > 0 and height > 0 else None
     raise ValueError(f"{func}: When selecting only a single resolution, provide"
                      "a Resolution or both width AND height!")
